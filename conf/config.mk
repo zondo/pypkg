d59e9fb71231064a939586e7e687708f031ac8b4
Glenn Hutchings <glenn.hutchings@genomicsengland.co.uk>
2022-03-30 11:29:06 +0100

    Fix issues on Windows
    - Change py to python (py doesn't work in venv)
    - Handle missing 'column' command

--- conf/config.mk
+++ conf/config.mk
@@ -1,24 +1,33 @@
 # Common config stuff.
 
 # Detect operating system.
-OS = $(shell uname)
+OS = $(shell uname -o)
 
-ifeq (${OS}, Linux)
+ifeq ($(OS), Linux)
 PYTHON = python3
 OSNAME = linux
 LINUX = true
-else ifeq (${OS}, Darwin)
+else ifeq ($(OS), Darwin)
 PYTHON = python3
 OSNAME = mac
 MAC = true
-else ifeq (${OS}, Msys)
-PYTHON = py -3
+else ifeq ($(OS), Msys)
+PYTHON = python
 OSNAME = windows
 WINDOWS = true
+else
+$(error Unknown OS: $(OS))
+endif
+
+# Set help column formatting.
+ifeq ($(OSNAME), windows)
+COLFMT = cat
+else
+COLFMT = column -t -s:
 endif
 
 help: ## This help message
 	@ echo "Usage: make [target]"
 	@ echo
 	@ grep -h ":.*##" $(MAKEFILE_LIST) | grep -v 'sed -e' | \
-	  sed -e 's/:.*##/:/' | column -t -s:
+	  sed -e 's/:.*##/:/' | $(COLFMT)
